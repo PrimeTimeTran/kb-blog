044fe7b8919459434422559e66a0f7582c5ddf3c
PrimeTimeTran <primetimetran@gmail.com>
2026-05-29 20:13:52 -0400

    Remove old sidebar and update lib index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,11 @@
 export * from './types';
-
 export * from './paths';
+
 export * from './trace';
 export * from './theme';
 export * from './build-scope';
 export * from './generate-rss';
 export * from './scroll';
-export * from './sort-tree';
 export * from './syntax-registry';
 export * from './remark';
 export * from './utils/formate-date';
