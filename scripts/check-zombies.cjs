03c51fec40f27c85be22de82206e7cb5340f4f7d
PrimeTimeTran <primetimetran@gmail.com>
2026-05-31 09:40:48 -0400

    Fix build

--- scripts/check-zombies.cjs
+++ scripts/check-zombies.cjs
@@ -1,6 +1,12 @@
-const why = require('why-is-node-running');
+const why = require('why-is-node-running').default;
+
+// If 'why' is an object that contains the function, you might need:
+// const why = require('why-is-node-running').default || require('why-is-node-running');
 
 console.log('Build finished, checking for active processes...');
 
-// This logs what is keeping the Node process alive
+// Most versions of this library export a function directly.
+// If it still fails, log the module to see what it contains:
+// console.log(why);
+
 why();
